d8f963535794c00672d40723496df3f09d79cc20
dile <duyle131103@gmail.com>
2025-08-17 12:59:53 +0700

    feat: implement instructor dashboard API

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -1,9 +1,9 @@
-import { createAccount, createNew, signIn, verifyToken } from "../services/instructorService.js";
+import { createAccount, createNew, deleteStudentByPhone, getAllDetails, signIn, updateDetails, verifyToken } from "../services/instructorService.js";
 
 export const addStudent = async (req, res) => {
   try {
-    await createNew(req.body);
-    res.status(201).json({ message: "Student added successfully" });
+    const newStudent = await createNew(req.body);
+    res.status(201).json({ message: "Student added successfully", student: newStudent });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -31,6 +31,33 @@ export const signInStudent = async (req, res) => {
   try {
     const students = await signIn(req.body);
     res.status(200).json({ message: "Sign in successful", students });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const getAllStudents = async (req, res) => {
+  try {
+    const students = await getAllDetails();
+    res.status(200).json({ message: "Students fetched successfully", students });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const updateStudent = async (req, res) => {
+  try {
+    const updatedStudent = await updateDetails(req.params.phone, req.body);
+    res.status(200).json({ message: "Student updated successfully", student: updatedStudent });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const deleteStudent = async (req, res) => {
+  try {
+    await deleteStudentByPhone(req.params.phone);
+    res.status(200).json({ message: "Student deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
