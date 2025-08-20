c074b02672f16dc67e63b0fbaeadd4db13fd368a
dile <duyle131103@gmail.com>
2025-08-21 00:15:49 +0700

    feat: update lesson update API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
-import config from '../configs/config.js';
+import { doc, setDoc } from 'firebase/firestore';
 import { clearAccessCode, getAccessCode, storeAccessCode } from '../services/firebaseService.js';
 import jwt from 'jsonwebtoken';
+import { db } from '../configs/firebase.js';
+import config from '../configs/config.js';
+import bcrypt from 'bcrypt';
 
 export const createAccessCode = async (req, res) => {
   try {
@@ -16,6 +19,26 @@ export const validateAccessCode = async (req, res) => {
     const { phone, email } = req.body;
     await getAccessCode(req.body);
     await clearAccessCode(phone || email);
+
+    if (phone) {
+      const token = jwt.sign({ phone: phone }, config.jwtSecret, { expiresIn: '1h' });
+      const hashPassword = await bcrypt.hash('12345678', 10);
+      await setDoc(doc(db, "users", phone),
+      {
+        phone,
+        username: phone,
+        email: '',
+        token,
+        password: hashPassword,
+        role: 'instructor',
+        isVerified: true,
+        address: ''
+      }, { merge: true });
+      return res.status(200).json({
+        message: "Access code validated successfully",
+        token,
+      });
+    }
     res.status(200).json({ message: "Access code validated successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
--- routes/instructorRoute.js
+++ routes/instructorRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody.js";
 import { studentValidation } from "../validations/studentValidation.js";
-import { addStudent, assignLesson, createLesson, deleteStudent, getAllLessons, getAllStudents, setupAccount, signInStudent, updateStudent, verifySetupToken } from "../controllers/instructorController.js";
+import { addStudent, assignLesson, createLesson, deleteStudent, getAllLessons, getAllStudents, setupAccount, signInStudent, updateLesson, updateStudent, verifySetupToken } from "../controllers/instructorController.js";
 import { lessonValidation } from "../validations/lessonValidation.js";
 
 const router = Router();
@@ -16,6 +16,7 @@ router.post("/signIn", signInStudent);
 router.post("/createLesson", validateBody(lessonValidation), createLesson);
 router.post("/assignLesson", assignLesson);
 
+router.put("/editLesson/:id", updateLesson);
 router.put("/editStudent/:phone", updateStudent);
 router.delete("/student/:phone", deleteStudent);
 
